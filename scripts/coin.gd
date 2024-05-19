6ae81bff73f47265b81afd8ee5763c9a5bc4e335
Bryan Gomes <bryanpgomes@gmail.com>
2024-05-19 13:48:39 -0400

    Modify coin animations

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,10 +1,11 @@
 extends Area2D
 @onready var game_manager = %"Game Manager"
+@onready var animation_player = $AnimationPlayer
 
 
 #signals allow you to trigger a signal
 #masks are what the item receives, layer is where the item is located
 
 func _on_body_entered(body):
-	game_manager.add_point() 
-	queue_free()
+	game_manager.add_point()
+	animation_player.play("pickup")
