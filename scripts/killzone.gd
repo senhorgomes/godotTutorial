ba77fdcba1542a8f14ec173b133d5828a54e25f6
Bryan Gomes <bryanpgomes@gmail.com>
2024-05-18 23:35:18 -0400

    Create timer and reset game logic. Reorganize file structure

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,5 +1,12 @@
 extends Area2D
 
+@onready var timer = $Timer
 
 func _on_body_entered(body):
-	queue_free()
+#Timer is established in the inspector tool
+	timer.start()
+
+
+func _on_timer_timeout():
+#When the timer is done, this resets the game
+	get_tree().reload_current_scene()
