5ea230f1ec3f54ba3caf9739f9f6eed88f7a6e27
Bryan Gomes <bryanpgomes@gmail.com>
2024-05-19 12:52:02 -0400

    Create jump animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,22 +10,38 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	#
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# This gets the direction which is either -1, 0, 1
+	var direction = Input.get_axis("Move left", "Move Right")
 	if direction == 1:
 		animated_sprite_2d.flip_h = false
 		
 	if direction == -1:
 		animated_sprite_2d.flip_h = true
+		
+	#if player is not on the floor then render animation
+	#Play animation
+	if is_on_floor():
+		
+		if direction == 0:
+		#This will say that when the direction is stopped
+			#We will play the idle animation
+			animated_sprite_2d.play("Idle")
+		else:
+			animated_sprite_2d.play("Run")
+			
+	else:
+		animated_sprite_2d.play("Jump")
 		
 	if direction:
 		velocity.x = direction * SPEED
