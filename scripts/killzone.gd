2ce0904856b506f51f5238d4f934e66bc82e203b
Bryan Gomes <bryanpgomes@gmail.com>
2024-05-19 12:25:34 -0400

    Player death animation created

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,6 +4,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 #Timer is established in the inspector tool
 	timer.start()
 
